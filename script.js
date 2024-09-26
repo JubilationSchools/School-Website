2072e7570075b7b706bd26e8872f7737577178a9
Jubilation Schools Tanzania <jubilationschoolstz@gmail.com>
2024-09-26 15:27:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function learnMore() {
   window.location.href = "#contact";
 }
+
+document.querySelectorAll('.download-btn').forEach(button => {
+  button.addEventListener('click', (e) => {
+      const link = e.target.closest('.brochure').querySelector('iframe').src;
+      window.open(link, '_blank');
+  });
+});
